9029700d06fd222537fb7a706e0dafda81e5d2ab
bs0132 <david.pavo@alumnos.upm.es>
2025-05-20 15:52:09 +0200

    allow ips

--- middleware/SSR-master-server-main/app.js
+++ middleware/SSR-master-server-main/app.js
@@ -1,20 +1,33 @@
-var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var serveIndex = require('serve-index');
-var crypto = require('crypto');
-var { Buffer } = require('node:buffer');
-
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var ipFilter = require('express-ipfilter').IpFilter;
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+let ips = ['10.100.0.119', '10.50.0.0/16'];
+
+let clientIp = function(req, res){
+  return req.headers['x-forwarded-for'] ? (req.headers['x-forwarded-for']).split(',')[0] : "";
+}
+
+app.use(
+  ipFilter(
+    ips,
+    {
+     detectIp: clientIp,
+     mode: 'allow'
+    },
+  )
+)
 
 app.use(express.raw())
 
@@ -29,8 +42,6 @@ app.use('/users', usersRouter);
 app.use('/logs', serveIndex(path.join(__dirname, 'public/logs'))); // shows you the file list
 app.use('/logs', express.static(path.join(__dirname, 'public/logs'))); // serve the actual files
 
-//to include images
-// app.use('/images', express.static('images'));
 
 // catch 404 and forward to error handler
 // error handler
